4ff7e5570df509281b7973e55bee12c051022de6
maxdu1998 <50428941+maxdu1998@users.noreply.github.com>
2019-12-08 16:33:28 -0300

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+namespace Sistema {
+ class DB {
+  private Paciente[] pacientes = new Paciente[20];
+  private Consulta[] consultas = new Consulta[20];
+  public static Medico[] medicos = new Medico[20];
+
+  public DB() {
+
+  }
+
+ }
+}
